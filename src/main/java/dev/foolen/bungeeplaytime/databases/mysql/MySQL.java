b83e3bfb4da43f523e355bf269aa688f1046a5ec
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2019-08-14 19:08:40 +0200

    Update MySQL.java

--- src/main/java/dev/foolen/bungeeplaytime/databases/mysql/MySQL.java
+++ src/main/java/dev/foolen/bungeeplaytime/databases/mysql/MySQL.java
@@ -1,5 +1,8 @@
 package dev.foolen.bungeeplaytime.databases.mysql;
 
+import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import dev.foolen.bungeeplaytime.config.Config;
@@ -30,14 +33,28 @@ public class MySQL {
 	}
 
 	private void connect() {
-		datasource = new HikariDataSource();
-		String connectionUrl = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE;
-
-		datasource.setMaximumPoolSize(10);
-		datasource.setDriverClassName("com.mysql.jdbc.Driver");
-		datasource.setJdbcUrl(connectionUrl);
-		datasource.setUsername(USERNAME);
-		datasource.setPassword(PASSWORD);
+		Properties props = new Properties();
+		props.setProperty("jdbcUrl", "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE);
+		props.setProperty("username", USERNAME);
+		props.setProperty("password", PASSWORD);
+		
+		props.setProperty("dataSource.dataSourceClassName", "com.mysql.cj.jdbc.MysqlDataSource");
+		props.setProperty("dataSource.maximumPoolSize", "10");
+		props.setProperty("dataSource.cachePrepStmts", "true");
+		props.setProperty("dataSource.prepStmtCacheSize", "250");
+		props.setProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+		props.setProperty("dataSource.useServerPrepStmts", "true");
+		props.setProperty("dataSource.useLocalSessionState", "true");
+		props.setProperty("dataSource.rewriteBatchedStatements", "true");
+		props.setProperty("dataSource.cacheResultSetMetadata", "true");
+		props.setProperty("dataSource.cacheServerConfiguration", "true");
+		props.setProperty("dataSource.elideSetAutoCommits", "true");
+		props.setProperty("dataSource.maintainTimeStats", "false");
+				
+		HikariConfig hconfig = new HikariConfig(props);
+		
+		datasource = new HikariDataSource(hconfig);
+		datasource.setLeakDetectionThreshold(60 * 1000); // REMOVE
 	}
 
 	private void loadDatabases() {
